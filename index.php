82385c3e4a20c64ebb4db9146e3b58c3a84c2699
xandronx <xandronx@gmail.com>
2017-02-27 18:31:55 +0300

    second

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hi,it is a test site</title>
+    </head>
+    <body>
+      <?php
+  echo 'Hello, world!'; 
+?> 
+    </body>
+    <footer>
+        copiryght
+    </footer>
+</html>
+
+    
